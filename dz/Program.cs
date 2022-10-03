ae06182e2c0cbcd105a76807979829ca4c61a04b
Roman <romangushchintlt@gmail.com>
2022-10-03 21:55:05 +0400

    Добавил код программы

--- dz/Program.cs
+++ dz/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿string[] array1 = new string[] {"hello", "2", ":-)", "2222"};
+string[] array2 = new string[array1.Length];
+
+void LessThreeCharactersArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+LessThreeCharactersArray(array1, array2);
+PrintArray(array2);
